b08b1b0e0d04297963e10d84c0b3682b519d48bb
Donncha Redmond <dev@donncha.info>
2021-02-11 18:58:10 -0800

    DEV-25 Updated definition for format to :js

--- lib/action_view/template/handlers/rjs.rb
+++ lib/action_view/template/handlers/rjs.rb
@@ -3,7 +3,7 @@ module ActionView
     class RJS
       # Default format used by RJS.
       class_attribute :default_format
-      self.default_format = Mime[:js]
+      self.default_format = :js
 
       def call(template,source)
         "update_page do |page|;#{source}\nend"
